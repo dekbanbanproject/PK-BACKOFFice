80da1e31cf9809692ace506b50b56da94a15bce8
dekbanbanproject <dekbanbanproject@gmail.com>
2023-08-11 07:57:12 +0700

    dd

--- app/Http/Controllers/AutoController.php
+++ app/Http/Controllers/AutoController.php
@@ -188,19 +188,23 @@ class AutoController extends Controller
         // }       
 
             $data_sits = DB::connection('mysql3')->select('
-                SELECT o.an,o.vn,p.hn,p.cid,o.vstdate,o.vsttime,o.pttype,concat(p.pname,p.fname," ",p.lname) as fullname,o.staff,op.name as staffname,p.hometel
-                    ,pt.nhso_code,o.hospmain,o.hospsub,o.main_dep,v.income-v.discount_money-v.rcpt_money debit
+                SELECT o.an,o.vn
+                    
+                    ,pt.nhso_code,o.hospmain,o.hospsub
+                    ,o.staff,op.name as staffname,p.hometel
+                    ,o.main_dep,v.income-v.discount_money-v.rcpt_money debit
                     FROM ovst o
                     LEFT JOIN vn_stat v on v.vn = o.vn
                     join patient p on p.hn=o.hn
                     JOIN pttype pt on pt.pttype=o.pttype
                     JOIN opduser op on op.loginname = o.staff
-                    WHERE o.vstdate BETWEEN "2023-07-07" AND "2023-07-10"
+                    WHERE o.vstdate = "2023-07-11" 
                     AND o.main_dep NOT IN("011","036","107")
                     AND o.pttype NOT IN("M1","M2","M3","M4","M5","M6","13","23")
                     group by o.vn
-                    limit 1000
+                    
             ');
+            // ,p.hn,p.cid,o.vstdate,o.vsttime,o.pttype,concat(p.pname,p.fname," ",p.lname) as fullname
             // AND o.pttype NOT IN("M1","M2","M3","M4","M5","M6","13","23")
             // AND o.main_dep NOT IN("011","036","107")
             // AND o.pttype NOT IN("M1","M2","M3","M4","M5","M6")
