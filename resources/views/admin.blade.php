332df8bef8930a05754debe98bee527a53b61d9b
dekbanbanproject <dekbanbanproject@gmail.com>
2024-04-09 22:00:52 +0700

    d

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -825,14 +825,14 @@
                                         <div class="flex-grow-1">
                                             <p class="text-start font-size-13 mb-2">PK-OFFICE</p>
                                             <a href="" target="_blank">
-                                                <h5 class="text-start mb-2">Tecnicial-Service</h5>
+                                                <h5 class="text-start mb-2">CCTV</h5>
                                             </a>
                                         </div>
                                         <div class="avatar ms-2">
                                             <a href="{{ url('techservice') }}" target="_blank">
                                                 <button
                                                     class="btn-icon btn-shadow btn-dashed btn btn-outline-info avatar-title bg-white text-primary rounded-pill">                                                         
-                                                        <img src="{{ asset('images/techserevice.png') }}" height="70px" width="70px" class="rounded-circle me-3"> 
+                                                        <img src="{{ asset('images/cctv1.png') }}" height="70px" width="70px" class="rounded-circle me-3"> 
                                                 </button>
                                             </a>
                                         </div>
